f98a86e4e1c2b5929e39bcba6dc81a5d0ce48e04
César Guerra <warderer@gmail.com>
2023-03-14 21:24:21 -0600

    ✨ Header con SASS + BEM

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
-import { Dashboard, Home, Login, Signup, Secret } from './pages'
+import { Header } from './components/Header'
+// import { Dashboard, Home, Login, Signup, Secret } from './pages'
 
 function App () {
   return (
     <div>
+      <Header />
       <h1> React Auth </h1>
-      <Home />
-      <Login />
-      <Signup />
-      <Dashboard />
-      <Secret />
     </div>
   )
 }
