d203ffa69d236cb1c06985f3b39e15078fdb154f
César Guerra <warderer@gmail.com>
2023-03-14 20:39:51 -0600

    ✨ Configuracion proyecto base

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { Dashboard, Home, Login, Signup, Secret } from './pages'
 
 function App () {
   return (
     <div>
       <h1> React Auth </h1>
+      <Home />
+      <Login />
+      <Signup />
+      <Dashboard />
+      <Secret />
     </div>
   )
 }
---
+++ 03-react-auth/src/pages/index.js
@@ -0,0 +1,13 @@
+import Dashboard from './Dashboard'
+import Home from './Home'
+import Login from './Login'
+import Signup from './Signup'
+import Secret from './Secret'
+
+export {
+  Dashboard,
+  Home,
+  Login,
+  Signup,
+  Secret
+}
