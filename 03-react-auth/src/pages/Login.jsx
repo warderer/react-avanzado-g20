51e933817c5280192353c1162a1c833ffc7d59cd
César Guerra <warderer@gmail.com>
2023-03-16 20:49:51 -0600

    ✨ Login Base con Bootstrap

--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -1,6 +1,42 @@
+import logo from '@/assets/react.svg'
+import '@/styles/form.css'
+
 const Login = () => {
   return (
-    <h1>Login</h1>
+    <main className='form-signin w-100 m-auto'>
+      <form>
+        <img className='mb-4' src={logo} alt='' width='72' height='57' />
+        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='floatingInput'
+            name='email'
+            value=''
+            placeholder='name@example.com'
+            onChange={() => {}}
+          />
+          <label htmlFor='floatingInput'>Email address</label>
+        </div>
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='floatingPassword'
+            name='password'
+            value=''
+            placeholder='Password'
+            onChange={() => {}}
+          />
+          <label htmlFor='floatingPassword'>Password</label>
+        </div>
+
+        <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
+      </form>
+    </main>
   )
 }
 export default Login
