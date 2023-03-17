8d487bf69d42d0f7c726e6dbeffccee8a716d41e
César Guerra <warderer@gmail.com>
2023-03-16 19:50:12 -0600

    ✨ Alias con Vite

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Header } from './components/Header'
+import { Header } from '@/components/Header'
 // import { Dashboard, Home, Login, Signup, Secret } from './pages'
 
 function App () {
