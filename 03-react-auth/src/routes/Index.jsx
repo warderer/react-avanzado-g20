ba93e405cbf3ceeec08c12d12d92ce158dbad919
César Guerra <warderer@gmail.com>
2023-03-21 20:54:22 -0600

    ✨ Protegiendo Dashboard con Auth

--- 03-react-auth/src/routes/Index.jsx
+++ 03-react-auth/src/routes/Index.jsx
@@ -1,11 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Dashboard, Home, Login, Signup, Secret } from '@/pages'
+import { useAuthContext } from '@/context/AuthContext'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route
+        path='/dashboard'
+        element={
+          isAuth
+            ? <Dashboard />
+            : <Navigate to='/login' replace />
+      }
+      />
       <Route path='/secret' element={<Secret />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
