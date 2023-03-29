4512e183e73310199b9b30781c0a8e87ee7d8df1
César Guerra <warderer@gmail.com>
2023-03-28 20:56:01 -0600

    ✨ Axios Interceptor para mandar Token

--- 03-react-auth/src/services/itemServices.js
+++ 03-react-auth/src/services/itemServices.js
@@ -2,10 +2,23 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3000'
 
+axios.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+},
+(error) => {
+  return Promise.reject(error)
+})
+
 const getSingleItem = (id) => axios.get(`${BASE_URL}/items/${id}`)
 const getAllItems = () => axios.get(`${BASE_URL}/items`)
+const createItem = (data) => axios.post(`${BASE_URL}/items`, data)
 
 export {
   getSingleItem,
-  getAllItems
+  getAllItems,
+  createItem
 }
